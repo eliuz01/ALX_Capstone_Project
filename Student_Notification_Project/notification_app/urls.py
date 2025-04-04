ebd9c3ad6b1ba588127197b0b0fabe1c9ce8004c
ELIUZ <osongoeliuz14@gmail.com>
2025-04-04 12:41:02 +0300

    added profile and customuser views

--- Student_Notification_Project/notification_app/urls.py
+++ Student_Notification_Project/notification_app/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import RegisterView, LoginView
-
+from .views import RegisterView, LoginView, ProfileUserRetrieveUpdateView, CustomUserListView, \
+CustomUserRetrieveUpdateDestroyView
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
-    path('login/', LoginView.as_view(), name='login')
+    path('login/', LoginView.as_view(), name='login'),
+    path('users/', CustomUserListView.as_view(), name='user-list'),
+    path('profile/', ProfileUserRetrieveUpdateView.as_view(), name='profile-detail'),
+    path('users/<int:student_id>/', CustomUserRetrieveUpdateDestroyView.as_view(), name='user-detail'),
 ]
--- Student_Notification_Project/notification_app/views.py
+++ Student_Notification_Project/notification_app/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from .models import CustomUser
-from .serializers import CustomUserSerializer, RegisterSerializer, LoginSerializer
-from rest_framework.views import APIView
+from .serializers import CustomUserSerializer, RegisterSerializer, LoginSerializer, ProfileUserSerializer
 from rest_framework.response import Response
 from rest_framework import status,generics
 from rest_framework.authtoken.models import Token
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
 
 # Create your views here.
 class RegisterView(generics.CreateAPIView):
@@ -19,14 +19,33 @@ class LoginView(generics.GenericAPIView):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         
-        # If valid, get the authenticated user object from the serializer's validated data
-        user = serializer.validated_data['user']
+        user = serializer.validated_data['user']        
+        token, created = Token.objects.get_or_create(user=user)        
         
-        # Retrieve or create a token for the authenticated user
-        token, created = Token.objects.get_or_create(user=user)
-        
-        # Return the token in the response
         return Response({'token': token.key}, status=status.HTTP_200_OK)
+
+#list all users in database
+class CustomUserListView(generics.ListAPIView):
+    queryset = CustomUser.objects.all()  # Get all users from the database
+    serializer_class = CustomUserSerializer
+
+  
+# View to retrieve, update, or delete a specific user by id
+class CustomUserRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = CustomUser.objects.all()  
+    serializer_class = CustomUserSerializer
+    permission_classes = [IsAdminUser]
+    lookup_field = 'student_id'  
+   
+#users can view and update their profiles
+class ProfileUserRetrieveUpdateView(generics.RetrieveUpdateAPIView):
+    queryset = CustomUser.objects.all()  # Get all users from the database
+    serializer_class = ProfileUserSerializer
+    permission_classes = [IsAuthenticated]  # Ensure that only authenticated users can access this view
+
+    def get_object(self):
+        # Return the currently authenticated user’s profile
+        return self.request.user  # This ensures users can only access their own profile
    
        
 
