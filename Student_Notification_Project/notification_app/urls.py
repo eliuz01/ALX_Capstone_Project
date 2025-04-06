2e8deae1f10040c657a0be6c886a3e36562a2b4e
ELIUZ <osongoeliuz14@gmail.com>
2025-04-06 05:15:45 +0300

    created URLs for Bus CRUD views

--- Student_Notification_Project/notification_app/urls.py
+++ Student_Notification_Project/notification_app/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .views import RegisterView, LoginView, ProfileUserRetrieveUpdateView, CustomUserListView, \
-CustomUserRetrieveUpdateDestroyView
+CustomUserRetrieveUpdateDestroyView, BusListCreateAPIView, BusRetrieveUpdateDestroyAPIView
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('users/', CustomUserListView.as_view(), name='user-list'),
     path('profile/', ProfileUserRetrieveUpdateView.as_view(), name='profile-detail'),
     path('users/detail/<int:student_id>/', CustomUserRetrieveUpdateDestroyView.as_view(), name='user-detail'),
+    path('buses/', BusListCreateAPIView.as_view(), name='bus-list-create'),
+    path('buses/<str:pk>/', BusRetrieveUpdateDestroyAPIView.as_view(), name='bus-retrieve-update-destroy'),
 ]
